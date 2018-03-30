6f15c74db51b39bcb808fd4f66f5f6ac1ad048fb
bouet <corentinbp@gmail.com>
2018-03-30 10:07:01 +0200

    Ajout type affranchissement et quantité

--- controllers/prest_controller.js
+++ controllers/prest_controller.js
@@ -1,7 +1,30 @@
 var db = require('../databases/db.js');
 
 exports.affr_create = function(req, res) {
-    res.render('prestations/affranchissements')
+    const query =  {
+        name: 'afficher-ligues',
+        text: 'select id, nom from ligue',
+    };
+    const query2 =  {
+        name: 'afficher-typeAffr',
+        text: 'select * from "typeAffr"',
+    };
+
+    db.get().query(query, function(err, result){
+        if (err) {
+            console.log(err.stack);
+            res.send('ERROR');
+        } else {
+            db.get().query(query2, function(err, result2){
+                if (err) {
+                    console.log(err.stack);
+                    res.send('ERROR');
+                } else {
+                    res.render('prestations/affranchissements', {listLigue : result.rows, typeAffr : result2.rows});
+                }
+            });
+        }
+    });
 };
 
 exports.repr_create = function(req, res) {
